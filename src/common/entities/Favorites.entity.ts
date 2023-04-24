8262a79020959f5c52fa29fdf8e906ad437946fe
iv4n89 <ibetanzos@aiudo.es>
2023-04-24 22:23:21 +0200

    fix favorites and wanted entities

--- src/common/entities/Favorites.entity.ts
+++ src/common/entities/Favorites.entity.ts
@@ -1,9 +1,10 @@
 import { Console } from 'src/console/entities/console.entity';
 import { Game } from 'src/game/entities/game.entity';
 import { User } from 'src/user/entities/user.entity';
-import { JoinColumn, ManyToOne } from 'typeorm';
+import { JoinColumn, ManyToOne, Entity } from 'typeorm';
 import { BaseEntity } from '../BaseEntity.entity';
 
+@Entity('favorites')
 export class Favorite extends BaseEntity {
   @ManyToOne(() => User, (user) => user.favorites)
   @JoinColumn({ name: 'user_id' })
